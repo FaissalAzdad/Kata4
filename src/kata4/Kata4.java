1fcd7ec009254ce9b1cf3237dbcd89633dadbc50
usuario <usuario@10.22.143.45>
2016-10-28 08:05:38 +0100

    Kata 4 versión 2

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -5,12 +5,18 @@ import java.io.File;
 public class Kata4 {
 
     public static void main(String[] args) {
-        File file = new File("C:\\");
-        String[] names = file.list();
+        File file = new File("C:\\pub");
+        print(file.listFiles(), "");
         
-        for (String name : names) {
-            System.out.println(name);
+        
+    }
+    private static void print(File[] files, String indent){
+           if(files == null) return; 
+           for (File file : files) {
+               System.out.println(indent + (file.isDirectory() ? "+" : "-")
+                       + file.getName());
+               if(!file.isDirectory() || file.isHidden()) continue;
+               print(file.listFiles(), " ");
         }
-        
     }
 }
